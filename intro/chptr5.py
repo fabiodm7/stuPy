8edbb8028d3d7580ad7e847bd553fd1a7a30a24a
fabiodm7 <fabio.d.m@hotmail.com>
2021-03-20 10:11:18 -0300

    commit on 'main'

--- intro/chptr5.py
+++ intro/chptr5.py
@@ -26,9 +26,7 @@ while x != 0:
 print('Fogo!')
 
 # imprima numeros de 1 até o número digitado pelo usuário, mas apenas números ímpares
-'''
 fim = int(input('Digite o ultimo numero da serie: '))
-'''
 x = 1
 while x <= fim:
     #print(x)
@@ -36,7 +34,7 @@ while x <= fim:
     if x % 2 != 0:
         print(x)
     x += 1
-'''
+
 # Reescreva o programa anterior para escrever os 10 primeiros multiplos de 3
 x = 1
 cont = 0
@@ -45,3 +43,56 @@ while x <= fim:
         print(x)
     x += 1
     cont += 1
+
+# ecreva um programa que imprima as linhsa de uma tabuada
+n = int(input('Tabuada do: '))
+x = 0
+while x <= 10:
+    print(f'{n} x {x} =', n*x)
+    x += 1
+
+# modifique o programa de modo que o usuário digite o início e o fim da tabuada
+n = int(input('Tabuada do: '))
+x = int(input('A partir de: '))
+y = int(input('Até: '))
+while x <= y:
+    print(f'{n} x {x} =', n*x)
+    x += 1
+
+# Escreva um programa que leia dois números. Imprima o resultado da multiplicação do
+# primeiro pelo segundo. Utilize apenas os operadores de soma e subtração para calcular
+# o resultado. Lembre-se que podemos entender a multiplicação de dois números como somas
+# sucessivas de um deles. Assim, 4 x 5 = 5 + 5 + 5 + 5 ou 4 + 4 + 4 + 4 + 4
+n = int(input('Deseja multiplicar: '))
+x = int(input('Por: '))
+y = 1
+aux = 0
+while y <= x:
+    aux += n
+    y += 1
+print(f'{n} x {x} = {aux}.')
+'''
+# Escreve um programa que leia dois números. Imprima a divisão inteira do primeiro pelo
+# segundo, assim como o resto da divisão. Utilize apenas operadores de soma e subtração
+# para chegar ao resultado. Lembre-se de que podemos entender o quociente da divisão de
+# de dois números como a quantidade de vezes que podemos retirar o divisor do dividendo.
+# Logo, 20/4 = 5, uma vez que podemos subtrair 4 cinco vezes de 20.
+n = int(input('Deseja dividir: '))
+x = int(input('Por: '))
+def divisao(n,x):
+    y = 1
+    aux = n
+    resto = 0
+    while aux > 0:
+        aux -= x
+        if aux >= x:
+            y += 1
+    print(f'{n} / {x} = {y}.')
+    if aux < x:
+        aux = aux + aux + aux + aux + aux + aux + aux + aux + aux + aux
+        while aux > 0:
+            aux -= x
+            if aux >= x:
+                resto += 1
+    print(f'{n} % {x} = {resto}.')
+divisao(n,x)
