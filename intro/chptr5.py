2c6ab41353e18ea28887c4a9e2517485a0ec2b64
fabiodm7 <fabio.d.m@hotmail.com>
2021-03-20 23:54:19 -0300

    commit on 'main'

--- intro/chptr5.py
+++ intro/chptr5.py
@@ -71,7 +71,7 @@ while y <= x:
     aux += n
     y += 1
 print(f'{n} x {x} = {aux}.')
-'''
+
 # Escreve um programa que leia dois números. Imprima a divisão inteira do primeiro pelo
 # segundo, assim como o resto da divisão. Utilize apenas operadores de soma e subtração
 # para chegar ao resultado. Lembre-se de que podemos entender o quociente da divisão de
@@ -79,21 +79,51 @@ print(f'{n} x {x} = {aux}.')
 # Logo, 20/4 = 5, uma vez que podemos subtrair 4 cinco vezes de 20.
 a = int(input('Deseja dividir: '))
 b = int(input('Por: '))
-# def divisao(n,x):
-#    y = 1
-#    aux = n
-#    resto = 0
-#    while aux > 0:
-#        aux -= x
-#        if aux >= x:
-#            y += 1
-#    print(f'{n} / {x} = {y}.')
-#    if aux < x:
-#        aux = aux + aux + aux + aux + aux + aux + aux + aux + aux + aux
-#        while aux > 0:
-#            aux -= x
-#            if aux >= x:
-#                resto += 1
-#    print(f'{n} % {x} = {resto}.')
-print(f'{a} / {b} = {div}.')
-print(f'{a} % {b} = {res}.')
+def divisao(n,x):
+    y = 1
+    z = 1
+    aux = n
+    if b == 0:
+        print('Não pode dividir por 0.')
+    else:
+        while aux > x:
+            aux -= x
+            if aux >= x:
+                y += 1
+        if aux < x and aux > 0:
+            aux = aux + aux + aux + aux + aux + aux + aux + aux + aux + aux
+            while aux > 0:
+                aux -= x
+                if aux >= x:
+                    z += 1
+        else:
+            z = 0
+        print(f'{a} / {b} = {y}.{z}')
+divisao(a,b)
+
+# contagem de questões corretas. o programa deve aceitar respostas com
+# letras maiúsculas e minúsculas
+pontos = 0
+questao = 1
+while questao <= 3:
+    resposta = input('Resposta da questao %d: ' % questao).lower()
+    if questao == 1 and resposta == 'b':
+        pontos += 1
+    if questao == 2 and resposta == 'a':
+        pontos += 1
+    if questao == 3 and resposta == 'd':
+        pontos += 1
+    questao += 1
+print('O aluno fez %d ponto(s).' % pontos)
+'''
+# Escreva um programa que pergunte o depósito inicial e a taxa de juros de uma poupança.
+# Exiba os valores mês a mês para os 24 primeiros meses.
+# Escreva o total de ganho com juros no período
+x = float(input('Depósito inicial de: '))
+tx = float(input('Taxa de juros: '))/100
+t = int(input('Anos de investimento: '))
+t0 = 1
+while t0 <= t:
+    x *= (1+tx)
+    t0 += 1
+print('Você acumulou %.2f.' % x)
