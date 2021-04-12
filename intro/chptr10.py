3c5138b34381b5f4ad6984b7de7d0757f125b213
fabiodm7 <fabio.d.m@hotmail.com>
2021-04-11 22:26:32 -0300

    main

--- intro/chptr10.py
+++ intro/chptr10.py
@@ -62,4 +62,169 @@ print('TV tamanho: ',tv.tamanho)
 print('TV marca: ',tv.marca)
 print('TV ligada: ',tv.ligada)
 print('TV Canal: ',tv.canal)
-'''
+
+# TEORIA: Verificação da faixa de canais
+class televisao:
+    def __init__(self,min,max):
+        self.ligada = False
+        self.canal = 2
+        self.tamanho = None
+        self.marca = None
+        self.cmin = min
+        self.cmax = max
+    def mudaCanalDiminui(self):
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+    def mudaCanalAumenta(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+
+tv = televisao(1,99)
+for x in range(0,120):
+    tv.mudaCanalAumenta()
+print(tv.canal)
+for x in range(0,120):
+    tv.mudaCanalDiminui()
+print(tv.canal)
+
+# Atualmente, a classe Televisão inicializa o canal com 2. Modifique a classe
+# televisão de forma a receber o canal inicial em seu construtor
+class televisao:
+    def __init__(self,min,max,c0):
+        self.ligada = False
+        self.canal = c0
+        self.tamanho = None
+        self.marca = None
+        self.cmin = min
+        self.cmax = max
+    def mudaCanalDiminui(self):
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+    def mudaCanalAumenta(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+
+tv = televisao(1,99,5)
+print(tv.canal)
+
+# Modifique a classe televisão de forma que, se pedirmos para mudar o canal para baixo,
+# além do mínimo, ela vá para o canal máximo. Se mudarmos para cima, além do canal
+# máximo, que volte ao canal mínimo
+class televisao:
+    def __init__(self,min,max,c0):
+        self.ligada = False
+        self.canal = c0
+        self.tamanho = None
+        self.marca = None
+        self.cmin = min
+        self.cmax = max
+    def mudaCanalDiminui(self):
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+        else:
+            self.canal = self.cmax
+    def mudaCanalAumenta(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+        else:
+            self.canal = self.cmin
+
+tv = televisao(1,10,1)
+for x in range(0,20):
+    print(tv.canal)
+    tv.mudaCanalAumenta()
+
+# utilizando o que aprendemos com funções, modifique o construtor da classe televisão
+# de forma que min e max sejam parâmetros opcionais, onde min vale 2 e max vale 14,
+# caso outro valor não seja passado
+class televisao:
+    def __init__(self,cmin = 2,cmax = 14):
+        self.ligada = False
+        self.canal = cmin
+        self.tamanho = None
+        self.marca = None
+        self.cmin = cmin
+        self.cmax = cmax
+    def mudaCanalDiminui(self):
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+        else:
+            self.canal = self.cmax
+    def mudaCanalAumenta(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+        else:
+            self.canal = self.cmin
+
+tv = televisao()
+for x in range(0,20):
+    print(tv.canal)
+    tv.mudaCanalAumenta()
+
+# Utilizando a classe televisão modificada no exercício anterior, crie duas instâncias,
+# especificando o valor de min e max por nome
+tvMin = televisao(5)
+print('tvMin')
+for x in range(0,20):
+    print(tvMin.canal)
+    tvMin.mudaCanalAumenta()
+tvMax = televisao(2,19)
+print('tvMax')
+for x in range(0,20):
+    print(tvMax.canal)
+    tvMax.mudaCanalAumenta()
+
+# TEORIA: Classe clientes / Exemplo de banco
+class cliente:
+    def __init__(self,nome,telefone):
+        self.nome = nome
+        self.telefone = telefone
+
+# Programa teste.py que importa a classe cliente do arquivo cliente.py
+from cliente import cliente
+joao = cliente("Joao da Silva","11987577806")
+maria = cliente("Maria da Silva","11986677786")
+
+# Altere o programa teste.py de forma a importar a classe conta. Crie uma conta
+# correte para os clientes João e Maria
+
+from cliente import cliente
+from contas import conta
+
+joao = cliente("Joao da Silva","11987577806")
+maria = cliente("Maria da Silva","11986677786")
+
+ccJoaoMaria = conta([joao,maria],'00001',2000)
+
+ccJoaoMaria.resumo()
+ccJoaoMaria.saque(1000)
+ccJoaoMaria.resumo()
+ccJoaoMaria.saque(50)
+ccJoaoMaria.resumo()
+ccJoaoMaria.deposito(200)
+ccJoaoMaria.resumo()
+
+# TEORIA: Conta com registro de operações e extrato
+class conta:
+    def __init__(self,clientes,numero,saldo = 0):
+        self.saldo = 0
+        self.clientes = clientes
+        self.numero = numero
+        self.opercacoes = []
+        self.deposito(saldo)
+    def resumo(self):
+        print('CC número: %s Saldo: %10.2f' % (self.numero,self.saldo))
+    def saque(self,valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            self.opercacoes.append(['Saque',valor])
+    def deposito(self,valor):
+        self.saldo += valor
+        self.opercacoes.append(['Deposito',valor])
+    def extrato(self):
+        print('Extrato CC No.: %s\n' % self.numero)
+        for o in self.opercacoes:
+            print('%10s %10.2f' % (o[0],o[1]))
+        print('\n    Saldo: %10.2f\n' % self.saldo)
+
+# Modifique o programa para imprimir o extrato das contas
