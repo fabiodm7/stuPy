866ced6dce3debb180bd146d4d48ae8d1f9cc77d
fabiodm7 <fabio.d.m@hotmail.com>
2021-05-15 12:51:14 -0300

    main

--- intro/agenda.py
+++ intro/agenda.py
@@ -1,3 +1,4 @@
+from _typeshed import OpenBinaryModeUpdating
 import sys
 import pickle
 from functools import total_ordering
@@ -5,7 +6,7 @@ from functools import total_ordering
 def nulo_ou_vazio(texto):
     return texto == None or not texto.strip()
 
-def valida_faixa_inteiro(pergunta,inicio,fim,padra = None):
+def valida_faixa_inteiro(pergunta,inicio,fim,padrao = None):
     while True:
         try:
             entrada = input(pergunta)
@@ -65,4 +66,213 @@ class Nome:
     def __str__(self):
         return self.nome
     def __repr__(self):
-        return ''
+        return '<Classe {3} em 0x{0:x} Nome: {1} Chave: {2}>'.format(id(self),self.__nome,self.__chave,type(self).__name__)
+    def __eq__(self,outro):
+        return self.nome == outro.nome
+    def __lt__(self,outro):
+        return self.nome < outro.nome
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, valor):
+        if nulo_ou_vazio(valor):
+            raise ValueError('Nome não pode ser nulo ou em branco')
+        self.__nome = valor
+        self.__chave = Nome.CriaChave(valor)
+    @property
+    def chave(self):
+        return self.__chave
+    @staticmethod
+    def CriaChave(nome):
+        return nome.strip().lower()
+
+@total_ordering
+class TipoTelefone:
+    def __init__(self,tipo):
+        self.tipo = tipo
+    def __str__(self):
+        return '({0})'.format(self.tipo)
+    def __eq__(self,outro):
+        if outro is None:
+            return False
+        return self.tipo == outro.tipo
+    def __lt__(self,outro):
+        return self.tipo < outro.tipo
+class Telefone:
+    def __init__(self,numero,tipo=None):
+        self.numero = numero
+        self.tipo = tipo
+    def __str__(self):
+        if self.tipo != None:
+            tipo = self.tipo
+        else:
+            tipo = ''
+        return '{0} {1}'.format(self.numero,tipo)
+    def __eq__(self,outro):
+        return self.numero == outro.numero and ((self.tipo == outro.tipo) or (self.tipo == None or outro.tipo == None))
+    @property
+    def numero(self):
+        return self.__numero
+    @numero.setter
+    def numero(self,valor):
+        if nulo_ou_vazio(valor):
+            raise ValueError('Numero não pode ser None ou em Branco')
+        self.__numero = valor
+class Telefones(ListaUnica):
+    def __init__(self):
+        super().__init__(Telefone)
+class TiposTelefone(ListaUnica):
+    def __init__(self):
+        super().__init__(TipoTelefone)
+class DadoAgenda:
+    def __init__(self,nome):
+        self.nome = nome
+        self.telefones = Telefones()
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self,valor):
+        if type(valor) != None:
+            raise TypeError('nome deve ser uma instância da classe Nome')
+        self.__nome = valor
+    def pesquisaTelefone(self,telefone):
+        posicao = self.telefones.pesquisa(Telefone(telefone))
+        if posicao == -1:
+            return None
+        else:
+            return self.telefones[posicao]
+class Agenda(ListaUnica):
+    def __init__(self):
+        super().__init__(DadoAgenda)
+        self.tiposTelefone = TiposTelefone()
+    def adicionaTipo(self,tipo):
+        self.tiposTelefone.adiciona(TipoTelefone(tipo))
+    def pesquisaNome(self,nome):
+        if type(nome) == str:
+            nome = Nome(nome)
+        for dados in self.lista:
+            if dados.nome == nome:
+                return dados
+        else:
+            return None
+    def ordena(self):
+        super().ordena(lambda dado: str(dado.nome))
+class Menu:
+    def __init__(self):
+        self.opcoes = [['Sair',None]]
+    def adicionaOpcao(self,nome,funcao):
+        self.opcoes.append([nome,funcao])
+    def exibe(self):
+        print('====')
+        print('Menu')
+        print('====\n')
+        for i,opcao in enumerate(self.opcoes):
+            print('[{0}] - {1}'.format(i,opcao[0]))
+        print()
+    def execute(self):
+        while True:
+            self.exibe()
+            escolha = valida_faixa_inteiro('Escolha uma opção: ',0,len(self.opcoes)-1)
+            if escolha == 0:
+                break
+            self.opcoes[escolha][1]()
+class AppAgenda:
+    @staticmethod
+    def pede_nome():
+        return(input('Nome: '))
+    @staticmethod
+    def pede_telefone():
+        return(input('Telefone: '))
+    @staticmethod
+    def mostra_dados(dados):
+        print('Nome: %s' % dados.nome)
+        for telefone in dados.telefones:
+            print('Telefones: %s' % telefone)
+        print()
+    @staticmethod
+    def mostra_dados_telefone(dados):
+        print('Nome: %s' % dados.nome)
+        for i, telefone in enumerate(dados.telefones):
+            print('{0} - Telefone: {1}'.format(i,telefone))
+        print()
+    @staticmethod
+    def pede_nome_arquivo():
+        return(input('Nome do arquivo: '))
+    def __init__(self):
+        self.agenda = Agenda()
+        self.agenda.adicionaTipo('Celular')
+        self.agenda.adicionaTipo('Residência')
+        self.agenda.adicionaTipo('Trabalho')
+        self.agenda.adicionaTipo('Fax')
+        self.menu = Menu()
+        self.menu.adicionaOpcao('Novo',self.novo)
+        self.menu.adicionaOpcao('Altera',self.altera)
+        self.menu.adicionaOpcao('Apaga',self.apaga)
+        self.menu.adicionaOpcao('Lista',self.lista)
+        self.menu.adicionaOpcao('Grava',self.grava)
+        self.menu.adicionaOpcao('Lê',self.le)
+        self.menu.adicionaOpcao('Ordena',self.ordena)
+        self.ultimo_nome = None
+    def pede_tipo_telefone(self,padrao=None):
+        for i,tipo in enumerate(self.agenda.tiposTelefone):
+            print(' {0} - {1} '.format(i,tipo,end=None))
+        t = valida_faixa_inteiro('Tipo: ',0,len(self.agenda.tiposTelefone)-1,padrao)
+        return self.agenda.tiposTelefone[t]
+    def pesquisa(self,nome):
+        dado = self.agenda.pesquisaNome(nome)
+        return dado
+    def novo(self):
+        novo = AppAgenda.pede_nome()
+        if nulo_ou_vazio(novo):
+            return
+        nome = Nome(novo)
+        if self.pesquisa(nome) != None:
+            print('Nome já existe!')
+            return
+        registro = DadoAgenda(nome)
+        self.menu_telefones(registro)
+        self.agenda.adiciona(registro)
+    def apaga(self):
+        if len(self.agenda) == 0:
+            print('Agenda vazia')
+        nome = AppAgenda.pede_nome()
+        if nulo_ou_vazio(nome):
+            return
+        p = self.pesquisa(nome)
+        if p != None:
+            self.agenda.remove(p)
+            print('Apagado. A agenda agora possui: %d registros' % len(self.agenda))
+        else:
+            print('Nada encontrado')
+    def altera(self):
+        if len(self.agenda) == 0:
+            print('Agenda vazia')
+        nome = AppAgenda.pede_nome()
+        if nulo_ou_vazio(nome):
+            return
+        p = self.pesquisa(nome)
+        if p != None:
+            print('\nEncontrado:\n')
+            AppAgenda.mostra_dados(p)
+            print('Digite enter caso queira alterar o nome')
+            novo = AppAgenda.pede_nome()
+            if not nulo_ou_vazio(novo):
+                p.nome = Nome(novo)
+            self.menu_telefones(p)
+        else:
+            print('Nome não encontrado!')
+    def menu_telefones(self,dados):
+        while True:
+            print('\nEditando telefones\n')
+            AppAgenda.mostra_dados_telefone(dados)
+            if len(dados.telefones) > 0:
+                print('\n[A] - Alterar\n[D] - apagar\n',end='')
+            print('[N] - novo\n[S] - sair\n')
+            operacao = input('Escolha uma operacao: ')
+            operacao = operacao.lower()
+            if operacao not in ['a','d','n','s']:
+                print('Operação invalida. Digite A, D, N ou S')
+                continue
+            
