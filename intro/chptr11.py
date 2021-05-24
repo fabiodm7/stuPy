5fd8ff95a0fd65ce248df6d59d29a9ea879d9085
fabiodm7 <fabio.d.m@hotmail.com>
2021-05-23 21:37:29 -0300

    main

---
+++ intro/chptr11.py
@@ -0,0 +1,24 @@
+'''
+Exercícios do capítulo 11 do livro "Introdução ao python"
+de Nilo Ney Coutinho Menezes
+
+Banco de dados
+'''
+# TEORIA: Exemplo de uso do SQLite em Python
+import sqlite3
+
+conexao = sqlite3.connect('agenda.db')
+cursor = conexao.cursor()
+cursor.execute('''
+    create table agenda (
+        nome text,
+        telefone text
+    )
+''')
+cursor.execute('''
+    insert into agenda (nome, telefone)
+        values(?,?)
+''',('Nilo','97788-1432'))
+conexao.commit()
+cursor.close()
+conexao.close()
