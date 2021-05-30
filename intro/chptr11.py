e17fb1863e0c65067d6ae7901546062e905e2ee8
fabiodm7 <fabio.d.m@hotmail.com>
2021-05-30 20:15:30 -0300

    main

--- intro/chptr11.py
+++ intro/chptr11.py
@@ -4,21 +4,33 @@ de Nilo Ney Coutinho Menezes
 
 Banco de dados
 '''
+'''
 # TEORIA: Exemplo de uso do SQLite em Python
 import sqlite3
 
 conexao = sqlite3.connect('agenda.db')
 cursor = conexao.cursor()
-cursor.execute('''
+cursor.execute(''''''
     create table agenda (
         nome text,
         telefone text
     )
-''')
-cursor.execute('''
+'''''')
+cursor.execute(''''''
     insert into agenda (nome, telefone)
         values(?,?)
-''',('Nilo','97788-1432'))
+'''''',('Nilo','97788-1432'))
 conexao.commit()
 cursor.close()
 conexao.close()
+
+# TEORIA: Consulta
+import sqlite3
+conexao = sqlite3.connect('agenda.db')
+cursor = conexao.cursor()
+cursor.execute('select * from agenda')
+resultado = cursor.fetchone()
+print('Nome: %s\nTelefone: %s' % (resultado))
+cursor.close()
+conexao.close()
+'''
